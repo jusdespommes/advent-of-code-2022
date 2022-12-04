921c431707dc3e3cfb7ce08f53e558ded4cf88b0
Alex <jusdespommes@users.noreply.github.com>
2022-12-04 11:30:02 +0000

    20221204

--- day-3.py
+++ day-3.py
@@ -14,17 +14,5 @@ data['pack_count'] = data['data'].apply(lambda x: len(x))
 
 data['compartment_size'] = data['pack_count']/2
 
-print(data.head())
-
-split_row = []
-
-
-def split_str_by_size(col, size):
-    new = col.str.split(size, expand=True)
-    split_new = split_row.append(new)
-    return split_new
-
-
-data.apply(lambda x: split_str_by_size(data['data'], data['compartment_size']))
-
+data['1'] = data.apply(lambda x: x['data'].str[0:x['compartment_size']])
 print(data)
